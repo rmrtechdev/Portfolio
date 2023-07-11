059efb993bbdda7d5125239013b8bef8ae2a8987
levii <heikojan2010@ymail.com>
2023-07-11 08:13:00 -0400

    starlink plugged, need iss as marker

--- src/GlobeEncom/MyGlobe.js
+++ src/GlobeEncom/MyGlobe.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { EncomGlobe } from 'encom-globe-react'
 import 'encom-globe-react/dist/index.css'
 import './myglobe.css'
+import starlink1 from './starlink1.json'
 
 const marker1 = { lat: 49.25, lon: -123.1, label: "Vancouver" };
 const marker2 = { lat: 35.6895, lon: 129.69171, label: "Tokyo", connected: true };
@@ -17,12 +18,12 @@ export default function MyGlobe(props) {
 
     useEffect(() => window.scrollTo(0, 0), [])
 
-    const initialSize = Math.min(window.innerWidth, window.innerHeight);
+    const initialSize = Math.min(window.outerWidth, window.outerHeight);
     const [state, setState] = React.useState({ width: initialSize, height: initialSize });
     React.useEffect(() => {
 
         const cb = () => {
-            const newSize = Math.min(window.innerWidth, window.innerHeight);
+            const newSize = Math.min(window.outerWidth, window.outerHeight);
             setState({ width: newSize, height: newSize })
         };
         window.addEventListener('resize', cb, false);
@@ -30,7 +31,15 @@ export default function MyGlobe(props) {
         return () => window.removeEventListener('resize', cb)
     }, []);
 
-    
+    const strstarlink = JSON.stringify(starlink1)
+    const objD = JSON.parse(strstarlink); 
+
+    const modifiedData = objD.slice(0, 66).map(({ _id, id, height_km, ...rest }) => ({...rest,
+        altitude: height_km
+    }));
+
+
+
 
     const [markers, setMarkers] = React.useState([]);
     const [constellations, setConstellations] = React.useState([]);
@@ -46,15 +55,26 @@ export default function MyGlobe(props) {
         
         //ADD SATELLITES
         setTimeout(() => {
-            const constellation = [];
+           //const constellation = [];
             const opts = {
                 waveColor: "#FFF",
                 coreColor: "yellow",
                 shieldColor: "gold",
-                numWaves: 13
+                numWaves: 9,
+                size: .6
             };
-            const alt = 1.3;
 
+            
+            const constellation = modifiedData.map((item) => ({
+                 
+                    
+                        lat: item.latitude,
+                        lon: item.longitude,
+                        altitude: item.altitude/320
+                    
+                
+            }));
+/*
             for (let i = 0; i < 2; i++) {
                 for (let j = 0; j < 3; j++) {
                     constellation.push({
@@ -64,6 +84,8 @@ export default function MyGlobe(props) {
                     });
                 }
             }
+            */
+            
             
             setConstellations([{
                 opts,
@@ -74,16 +96,19 @@ export default function MyGlobe(props) {
 
     return (
         <EncomGlobe
-        
         width={state.width}
         height={state.height}
-        markers={markers}
-        constellations={constellations}
+        constellations= {constellations}
         globeReadyCb={demo}
             globeConfig={{
                 ...EncomGlobe.defaultProps.globeConfig,
                 baseColor: "#00e6e6",
-                pinColor: "#FF69B8"
+                pinColor: "#FF69B8",
+                viewAngle: .3,
+               
+
+
+                
             }}
     
     />
